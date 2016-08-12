c697062228d66ba4a104a0e21b24959564d25463
naodell <Nathaniel.Jay.Odell@cern.ch>
2016-08-12 15:22:46 -0500

    Adding branch amumu_2012

--- BLTAnalysis/test/batch_cfg.py
+++ BLTAnalysis/test/batch_cfg.py
@@ -5,7 +5,8 @@ import sys
 
 ''' Specify parameters '''
 cfg        = bm.JobConfig
-path       = '/tthome/share/bacon/production'
+#path       = '/tthome/share/bacon/production/04'
+path       = '/eos/uscms/store/user/naodell/bacontuples'
 executable = 'execBatch.sh'
 selection  = 'amumu'
 period     = '2012'
@@ -18,22 +19,22 @@ period     = '2012'
 dataList = []
 dataList.extend([
     cfg(dataName = 'muon_2012A',
-        path     = '{0}/04/SingleMu_2012A-22Jan2013'.format(path),
+        path     = '{0}/SingleMu_2012A-22Jan2013'.format(path),
         nJobs    = 1,
         args     = 'muon_2012A muon 2012'
        ),
     cfg(dataName = 'muon_2012B',
-        path     = '{0}/04/SingleMu_2012B-22Jan2013'.format(path),
+        path     = '{0}/SingleMu_2012B-22Jan2013'.format(path),
         nJobs    = 10,
         args     = 'muon_2012B muon 2012'
        ),
     cfg(dataName = 'muon_2012C',
-        path     = '{0}/04/SingleMu_2012C-22Jan2013'.format(path),
+        path     = '{0}/SingleMu_2012C-22Jan2013'.format(path),
         nJobs    = 10,
         args     = 'muon_2012C muon 2012'
        ),
     cfg(dataName = 'muon_2012D',
-        path     = '{0}/04/SingleMu_2012D-22Jan2013'.format(path),
+        path     = '{0}/SingleMu_2012D-22Jan2013'.format(path),
         nJobs    = 10,
         args     = 'muon_2012D muon 2012'
        )

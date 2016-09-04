e2a11aa01c7fbebc54c5e0d88b4df3ba2af82f66
naodell <naodell@gmail.com>
2016-09-04 10:06:07 -0500

    Removing plots directory and merging python directory with amumu_2016

--- BLTAnalysis/test/execBatch.sh
+++ BLTAnalysis/test/execBatch.sh
@@ -15,10 +15,22 @@ PERIOD=$5
 ### Transfer files, prepare directory ###
 TOPDIR=$PWD
 
+# lpc
+#export SCRAM_ARCH=slc6_amd64_gcc491
+#export CMSSW_VERSION=CMSSW_7_4_14
+#source /cvmfs/cms.cern.ch/cmsset_default.sh
+
+# nut3
 export SCRAM_ARCH=slc6_amd64_gcc491
 export CMSSW_VERSION=CMSSW_7_4_12
-source /software/tier3/osg/cmsset_default.sh
+source /software/tier3/osg/cmsset_default.sh 
 
+# Setup CMSSW environment
+#scram project CMSSW $CMSSW_VERSION
+#cd $CMSSW_VERSION/src
+#eval `scram runtime -sh`
+
+#cp $TOPDIR/source.tar.gz .
 tar -xzf source.tar.gz
 cd $CMSSW_VERSION/src/
 cmsenv
